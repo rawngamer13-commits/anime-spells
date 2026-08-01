94a1375eb41abfabe7d02857d674ccfc89a564a5
rawngamer13-commits <rawngamer13@gmail.com>
2026-08-01 17:17:02 -0400

    Update main mod class with entity registration

--- src/main/java/com/rawngamer13/animespells/AnimespellsMod.java
+++ src/main/java/com/rawngamer13/animespells/AnimespellsMod.java
@@ -4,7 +4,12 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.registry.EntityRegistry;
+import net.minecraftforge.fml.relauncher.Side;
 import org.apache.logging.log4j.Logger;
+
+import com.rawngamer13.animespells.spells.DoomBlackHole;
+import com.rawngamer13.animespells.event.EntityRegisterHandler;
 
 @Mod(
     modid = AnimespellsMod.MODID,
@@ -23,12 +28,31 @@ public class AnimespellsMod {
     public static void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
         logger.info("Anime Spells initializing...");
+        
+        // Registrar entidades
+        EntityRegistry.registerModEntity(
+            DoomBlackHole.class,
+            "doom_black_hole",
+            0,
+            MODID,
+            64,
+            10,
+            true
+        );
+        
         SpellRegistry.registerSpells();
     }
 
     @Mod.EventHandler
     public static void init(FMLInitializationEvent event) {
         logger.info("Anime Spells initialized successfully!");
+        
+        // Registrar event handlers
+        if (event.getSide() == Side.CLIENT) {
+            net.minecraftforge.fml.common.FMLCommonHandler.instance().bus().register(
+                new com.rawngamer13.animespells.event.SkyRenderHandler()
+            );
+        }
     }
 
     @Mod.EventHandler
