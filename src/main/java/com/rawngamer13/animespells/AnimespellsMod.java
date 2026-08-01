7f5d2ecdeac2bb343070bfe6982f092bf204d8d7
rawngamer13-commits <rawngamer13@gmail.com>
2026-08-01 17:12:44 -0400

    Add main mod class

---
+++ src/main/java/com/rawngamer13/animespells/AnimespellsMod.java
@@ -0,0 +1,38 @@
+package com.rawngamer13.animespells;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(
+    modid = AnimespellsMod.MODID,
+    name = AnimespellsMod.NAME,
+    version = AnimespellsMod.VERSION,
+    dependencies = "required-after:wizardry"
+)
+public class AnimespellsMod {
+    public static final String MODID = "animespells";
+    public static final String NAME = "Anime Spells";
+    public static final String VERSION = "1.0.0";
+
+    public static Logger logger;
+
+    @Mod.EventHandler
+    public static void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+        logger.info("Anime Spells initializing...");
+        SpellRegistry.registerSpells();
+    }
+
+    @Mod.EventHandler
+    public static void init(FMLInitializationEvent event) {
+        logger.info("Anime Spells initialized successfully!");
+    }
+
+    @Mod.EventHandler
+    public static void postInit(FMLPostInitializationEvent event) {
+        logger.info("Anime Spells post-initialized!");
+    }
+}
